e5d06e25ba9f3044126fefc1c36213bde2853332
soumyaagr427 <soumya.agr06@gmail.com>
2021-08-25 22:28:17 +0530

    app with one screen

--- app/src/main/java/com/example/i_am_sorry/MainActivity.kt
+++ app/src/main/java/com/example/i_am_sorry/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.i_am_sorry
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun sendMessage(view: View)
+    {
+
+    }
+
+
 }
